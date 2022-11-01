4abda18ee282a059e81cd9bf657e379fe180b6de
zhangxin <zhangxin1@uino.com>
2022-11-01 23:24:28 +0800

    定制化解析excel

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import importExcel from "./import-excel";
+
+const PRODUCT_NAME = "商品名称";
+const BUSINESS_TYPE = "交易类型";
+const enumBusinessType = {
+  enter: "结算入账",
+  refund: "退款",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pres, setPres] = useState([]);
+  const importExcelCallback = (data) => {
+    let map = new Map();
+    data
+      .filter((item) => item[PRODUCT_NAME])
+      .map((item) => {
+        map.set(item[PRODUCT_NAME], { ...item, enterCount: 0, refundCount: 0 });
+        return item;
+      })
+      .map((item) => {
+        if (item[BUSINESS_TYPE] === enumBusinessType.enter) {
+          map.get(item[PRODUCT_NAME]).enterCount++;
+        }
+        if (item[BUSINESS_TYPE] === enumBusinessType.refund) {
+          map.get(item[PRODUCT_NAME]).refundCount++;
+        }
+      });
+    let arr = [];
+    for (let item of map.values()) {
+      arr.push(
+        <div className="container">
+          <p className="name">{item[PRODUCT_NAME]}</p>
+          <p className="enter">
+            <span>{item.enterCount}</span>
+          </p>
+          <p className="refund">
+            <span>{item.refundCount}</span>
+          </p>
+        </div>
+      );
+    }
+    setPres(arr);
+  };
+  useEffect(() => {}, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={(e) => {
+          importExcel(e, importExcelCallback);
+        }}
+      />
+      {pres.length > 0 && (
+        <div className="container">
+          <p className="name">{PRODUCT_NAME}</p>
+          <p className="enter">{enumBusinessType.enter}</p>
+          <p className="refund">{enumBusinessType.refund}</p>
+        </div>
+      )}
+      <div>{pres}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
